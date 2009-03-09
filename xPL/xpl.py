10610c291e3496f4aa4f0cc9da93f899e351635f
Gee Bee <mike.goldberger@gmail.com>
2009-03-09 05:57:43 +0000

    abstract the xPL code to xpl.py xplHandler class refactor xplpython.py to use the xplHandler class

---
+++ xPL/xpl.py
@@ -0,0 +1,94 @@
+from socket import *
+from SensorLookup import Sensor
+import re,select,sys
+import json
+import urllib
+
+class xplHandler:
+    message_buffer = 1500
+    messageDict = {}
+    jsonRequest = {}
+    xpl_port = 50002
+    computername = gethostname()
+    xpl_ip = gethostbyname(gethostname())
+    xpl_instance = "monitor." + computername
+    UDPSock = socket(AF_INET,SOCK_DGRAM)
+    addr = ("0.0.0.0",xpl_port)
+    
+    def __init__(self):
+    
+        try :
+            self.UDPSock.bind(self.addr)
+        except :
+            self.xpl_port = 50000
+            self.addr = (self.xpl_ip, self.xpl_port)
+            
+            try :
+                self.UDPSock.bind(self.addr)
+            except :
+                self.xpl_port += 1
+            
+        print "xPL Monitor bound to port: " + str(self.xpl_port)
+
+    def validInstance (self, computername):
+        newComputerName = re.sub('(-|\.|!|;)', '',  computername)
+        newComputerName = newComputerName[:16]
+        return newComputerName
+    
+    def sendXplMessage (self, msgType='xpl-stat', source='mikesha.monitor', target='*', appName='hbeat.app', interval=5, port=50002):
+        hbSock = socket(AF_INET,SOCK_DGRAM)
+        hbSock.setsockopt(SOL_SOCKET,SO_BROADCAST,1)
+        msg = msgType + "\n{\nhop=1\nsource=" + source + "\ntarget=" + target + "\n}\n" + appName + "\n{\ninterval=" + str(interval) + "\nport=" + str(port) + "\n}\n"
+        hbSock.sendto(msg,("255.255.255.255",3865))
+    
+    def startListener (self, UDPSock = None):
+        
+        self.computername = self.validInstance(self.computername)
+        
+        while UDPSock :
+            readable, writeable, errored = select.select([UDPSock],[],[],60)
+    
+            if len(readable) == 1 :
+                data,addr = UDPSock.recvfrom(self.message_buffer)
+                messageArray = data.splitlines()
+                try:
+                    for msgLine in messageArray:
+                        if msgLine == '{':
+                            messageArray.remove(msgLine)
+                        elif msgLine == '}':
+                            messageArray.remove(msgLine)
+                except:
+                    print "No Curly Braces To Remove"
+            
+                if messageArray[0] == 'xpl-trig':
+                    for msgLine in messageArray:
+                        if msgLine.find('=') != -1:
+                            kvArray = msgLine.partition('=')
+                            self.messageDict[kvArray[0]] = kvArray[2]
+        
+                    deviceId = re.match('([a-z|A-Z]+.*[0-9]+.*).([0-9]+[a-z]+)', self.messageDict['device'])
+                    sensorType = deviceId.group(1)
+                    sensorHexId = deviceId.group(2)
+        
+                    sensor = Sensor()
+                    ds10a = sensor.getSingleSensor('id',1)
+        
+        
+                    if self.messageDict['command'] == 'alert':               
+                        self.xplToJSON(ds10a['name'], 'alert')
+                        print "Sensor (Type:" + sensorType + ", ID: " + sensorHexId + ") is reporting its status as: OPEN"
+                    if self.messageDict['command'] == 'normal':
+                        self.xplToJSON(ds10a['name'], 'normal')
+                        print "Sensor (Type:" + sensorType + ", ID: " + sensorHexId + ") is reporting its status as: CLOSED"
+                    
+
+    def xplToJSON (self, jsId = None, sensorStatus = None):
+        
+        if jsId != None and sensorStatus != None:
+            jsonRequest = {'jsId': jsId, 'sensorStatus': sensorStatus}
+        
+            #http-post
+            params = urllib.urlencode(jsonRequest)
+            request = urllib.urlopen("http://localhost/cgi-bin/acceptJSON.cgi", params)
+        else:
+            print "You must pass the jsId and the sensorStatus variables"
